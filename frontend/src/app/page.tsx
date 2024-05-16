c41470ea08388445e0320339dc669225e1a7a5be
joieng1 <johncieng1@gmail.com>
2024-05-15 17:00:07 -0700

    Add Board to main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Box, SimpleGrid, Text } from "@chakra-ui/react";
 import Header from "./ui/header";
+import Board from "./components/Board";
 
 export default function Home() {
   return (
     <Box>
+      <Board />
       <Header />
       <Box p={[10, 10]}>
         <SimpleGrid columns={[1, null, 2]} spacingX='3rem'>
