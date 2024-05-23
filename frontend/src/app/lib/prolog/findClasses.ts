b5e17a1a184be6d1a621a557f6842f9a5855ec77
MrEminent42 <github@leohorwitz.com>
2024-05-22 17:35:42 -0700

    'use server' directive in the findClasses.ts (wherever we want to use Tau Prolog JS)

--- frontend/src/app/lib/prolog/findClasses.ts
+++ frontend/src/app/lib/prolog/findClasses.ts
@@ -1,6 +1,7 @@
-'use client';
+'use server';
 
 import { Course } from "@/app/components/Board";
+import TauProlog from "tau-prolog";
 
 export const findClasses = (taken: Course[]) => {
     // load prolog kb from file kb.pl
@@ -8,34 +9,35 @@ export const findClasses = (taken: Course[]) => {
     // let session = pl.create();
 
 
-    const pl = require("./tau-prolog");
+    // const pl = require("./tau-prolog");
     // const session = pl.create(1000);
+    const session = TauProlog.create(1000);
 
 
 
-    // session.consult("                   \
-    //     taken(jon). \
-    //     taken(thing2). \
-    // ", {
-    //     success: function () {
-    //         /* Program parsed correctly */
-    //         console.log("TAU LOAD SUCCESS!")
-    //     },
-    //     error: function (err: any) {
-    //         console.log("TAU LOAD ERROR!")
-    //         console.log(err)
-    //     },
-    // });
+    session.consult("                   \
+        taken(jon). \
+        taken(thing2). \
+    ", {
+        success: function () {
+            /* Program parsed correctly */
+            console.log("TAU LOAD SUCCESS!")
+        },
+        error: function (err: any) {
+            console.log("TAU LOAD ERROR!")
+            console.log(err)
+        },
+    });
 
-    // session.query("taken(X).", {
-    //     success: (goal: any) => {
-    //         console.log("QUERY SUCCESS!")
-    //         console.log(goal)
-    //     },
-    //     error: (err: any) => {
-    //         console.log("QUERY ERROR!")
-    //         console.log(err)
-    //     }
-    // })
+    session.query("taken(X).", {
+        success: (goal: any) => {
+            console.log("QUERY SUCCESS!")
+            console.log(goal)
+        },
+        error: (err: any) => {
+            console.log("QUERY ERROR!")
+            console.log(err)
+        }
+    })
 
 }
